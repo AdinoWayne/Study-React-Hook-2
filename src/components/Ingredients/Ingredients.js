ff08faee72d0c6aeb07991c35a3254a9ea06ddf9
Tran Quang Trung <trung.tran.connectiv@gmail.com>
2020-01-14 15:10:38 +0700

    [update] #issue468

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hook-update-c7bff.firebaseio.com//ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const filteredIngredientsHandler = useCallback(filteredIngredient => {
+    setUserIngredients(filteredIngredient)
+  }, []);
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
