f4adc1b7627004ff9a19d11f07a5f7f418847f82
AdinoWayne <gnurt10@gmail.com>
2020-01-17 00:36:55 +0700

    [update] handle error

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState();
 
   const addIngredientHandler = ingredient => {
+    setIsloading(true);
     fetch('https://react-hook-update-c7bff.firebaseio.com//ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     })
       .then(response => {
+        setIsloading(false);
         return response.json();
       })
       .then(responseData => {
@@ -28,19 +33,31 @@ const Ingredients = () => {
     setUserIngredients(filteredIngredient)
   }, []);
   const removeIngredientHandler = ingredientId => {
+    setIsloading(true);
     fetch(`https://react-hook-update-c7bff.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE',
     })
     .then(res => {
+      setIsloading(false);
       setUserIngredients(prevIngredients =>
         prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       );
     })
+    .catch(err => {
+      setError('Something went wrong!');
+      setIsloading(false);
+    });
   };
+
+  const clearError = () => {
+    setError(null);
+    setIsloading(false);
+  }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+     {error && <ErrorModal onClose={clearError} >{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
